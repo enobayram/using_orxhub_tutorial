799b64dad49c28a8a76e04d521450f3792bd3125
Enis Bayramoglu <enisbayramoglu@gmail.com>
2015-11-19 20:13:32 +0200

    Added the user repo package management script

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Orx Package Repository
 ======================
 
 This is a repository of orx packages. The packages are contained as branches in this repository and their names start with the prefix `package/`
+
+Installing Packages
+-------------------
+
+In any git repository (empty or otherwise), create an `orx_packages` file that contains the name of a package on each line. The names should not contain the `package/` prefix. Then 
+
+Then to install the packages listed in `orx_packages`, use the [package management script](orxpm.sh) at the repository root as:
+```
+./orxpm.sh <orxhub_repo_url>
+```
---
+++ orxpm.sh
@@ -0,0 +1,64 @@
+if [ "$#" -eq 1 ]; then
+    REMOTE=$1
+    mkdir -p .orxpm
+    echo $1 > .orxpm/remote
+elif [ -f .orxpm/remote ]; then
+    REMOTE=$(cat .orxpm/remote)
+    echo "Using the previous package repository: $REMOTE"
+else
+    >&2 echo "Please provide a remote repository address as the first argument"
+    exit 1
+fi
+
+mkdir -p .orxpm/workbench
+cd .orxpm/workbench
+git init > /dev/null
+
+echo "Updating the local package repository..."
+git fetch $REMOTE '+refs/heads/*:refs/heads/orxhub/*'
+
+echo "Resetting the local package repository state..."
+git branch -D empty >/dev/null 2>&1
+git checkout --orphan empty
+git reset --hard
+git clean -f -d
+
+echo "Constructing the platform from packages..."
+while read PACKAGE; do
+    git merge orxhub/package/$PACKAGE
+done < ../../orx_packages
+
+echo "Converting the platform to a monolithic commit..."
+git branch -D new_platform >/dev/null 2>&1
+git checkout --orphan new_platform
+
+if [ -f ../platform ]; then
+    PLATFORM=$(cat ../platform)
+    echo "The old platform commit was $PLATFORM"
+    git fetch ../../
+    git reset $PLATFORM
+    git add .
+fi
+
+echo "Creating the new platform commit..."
+git commit -m "$(echo "Creating a new platform with packages:"; cat ../../orx_packages)"
+NEW_PLATFORM=$(git rev-parse HEAD)
+echo $NEW_PLATFORM > ../platform
+
+cd ../..
+
+echo "Merging the new platform from commit $NEW_PLATFORM"
+git fetch .orxpm/workbench new_platform
+git merge $NEW_PLATFORM --no-edit
+
+
+echo "Committing the OrxPM metadata..."
+git add orx_packages .orxpm/platform .orxpm/remote
+
+echo $NEW_PLATFORM $(git rev-parse HEAD)
+
+if [ "$NEW_PLATFORM" == "$(git rev-parse HEAD)" ]; then
+    git commit -m "OrxPM metadata"
+else
+    git commit --amend --no-edit
+fi
